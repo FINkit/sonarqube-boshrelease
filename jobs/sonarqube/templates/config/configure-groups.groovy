28846ce994bc643b077513bccac976c2586607a8
Mark Cox <coxm@car03-04909m.local>
2018-06-29 14:04:16 +0100

    Add users

--- jobs/sonarqube/templates/config/configure-groups.groovy
+++ jobs/sonarqube/templates/config/configure-groups.groovy
@@ -3,28 +3,30 @@ import groovy.json.JsonSlurper
 // Script must be called from the script directory to pick up the imported Class.
 import SonarApiClient
 
-List<Tuple> buildGroupPermissionsData() {
+List<Tuple> buildGroupsData() {
     def jsonSlurper = new JsonSlurper()
-    def reader = new BufferedReader(new InputStreamReader(new FileInputStream("group-permissions.json"), "UTF-8"))
+    def reader = new BufferedReader(new InputStreamReader(new FileInputStream("groups.json"), "UTF-8"))
     def data = jsonSlurper.parse(reader)
 
-    List<Tuple> groupPermissions = []
+    List<Tuple> groups = []
 
-    data.groupPermissions.each {
-        Tuple groupPermissionsTuple = new Tuple(it.name, it.description, it.permissions)
-        System.out.println("Adding group permissions ${groupPermissionsTuple}")
-        groupPermissions << groupPermissionsTuple
+    data.groups.each {
+        Tuple groupsTuple = new Tuple(it.name, it.description, it.permissions)
+        System.out.println("Adding group ${groupsTuple}")
+        groups << groupsTuple
     }
 
-    return groupPermissions
+    return groups
 }
 
 def sonarApiGroupsUrl = SonarApiClient.sonarApiUrl + 'user_groups/create'
 def sonarApiPermissionsUrl = SonarApiClient.sonarApiUrl + 'permissions/add_group'
 
-for (groups in buildGroupPermissionsData()) {
-    def name = SonarApiClient.buildSingleValuedKeyPair('name', groups.get(0))
-    def description = SonarApiClient.buildSingleValuedKeyPair('description', groups.get(1))
+for (group in buildGroupsData()) {
+    System.out.println("Adding group ${group}")
+
+    def name = SonarApiClient.buildSingleValuedKeyPair('name', group.get(0))
+    def description = SonarApiClient.buildSingleValuedKeyPair('description', group.get(1))
     def queryValues = [name, description]
     def queryString = SonarApiClient.buildQueryString(queryValues.iterator())
     def querySucceeded = SonarApiClient.postQueryString(sonarApiGroupsUrl, queryString)
@@ -33,12 +35,12 @@ for (groups in buildGroupPermissionsData()) {
         System.exit(1)
     }
 
-    def permissions = groups.get(2).tokenize(',')
-
-    System.out.println("Adding permissions ${permissions}")
+    def permissions = group.get(2).tokenize(',')
 
     for (permission in permissions) {
-        def groupName = SonarApiClient.buildSingleValuedKeyPair('groupName', groups.get(0))
+        System.out.println("Adding permission ${permission}")
+
+        def groupName = SonarApiClient.buildSingleValuedKeyPair('groupName', group.get(0))
         def permissionsPair = SonarApiClient.buildSingleValuedKeyPair('permission', permission)
 
     	queryValues = [groupName, permissionsPair]
